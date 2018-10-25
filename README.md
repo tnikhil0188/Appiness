6fb06c0e41a6d8b9634d7ed819a474ed559af627
mavarma <mav.aravind@gmail.com>
2018-10-25 19:26:17 +0530

    angular task

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# Appiness
+# angular2-registration-login-example
+
+Angular 2+ Task Demo
+Steps to run the application:
+
+a. clone the application using: git clone https://github.com/tnikhil0188/Appiness.git
+b. run the command: npm install
+c. run the command: npm start
+d. then, hit the url in the browser like: http://localhost:3000
---
+++ app/register/register.component.ts
@@ -0,0 +1,41 @@
+﻿import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { AlertService, UserService } from '../_services/index';
+
+@Component({
+    moduleId: module.id,
+    templateUrl: 'register.component.html'
+})
+
+export class RegisterComponent {
+    model: any = {};
+    loading = false;
+    emailPattern ="[a-zA-Z0-9.-]{1,}@[a-zA-Z.-]{2,}[.]{1}[a-zA-Z]{2,}";
+    mobileNumberPattern = "^((\\+91-?)|0)?[0-9]{10}$";
+    passwordPattern = "^[a-zA-Z0-9]{8,}$";
+
+    options = [
+        { name: "Admin", value: 1 },
+        { name: "Member", value: 2 }
+      ]
+
+    constructor(
+        private router: Router,
+        private userService: UserService,
+        private alertService: AlertService) { }
+
+    register() {
+        this.loading = true;
+        this.userService.create(this.model)
+            .subscribe(
+                data => {
+                    this.alertService.success('Registration successful', true);
+                    this.router.navigate(['login']);
+                },
+                error => {
+                    this.alertService.error(error);
+                    this.loading = false;
+                });
+    }
+}
